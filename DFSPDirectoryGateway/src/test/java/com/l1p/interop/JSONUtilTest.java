20077a48afefe0b91cdc45a5e7a67dd4dcafe340
QuiggyB <bcs3009@gmail.com>
2016-08-18 08:47:49 -0500

    Updated unit test for JSONUtil to cover both positive and negative cases.

--- DFSPDirectoryGateway/src/test/java/com/l1p/interop/JSONUtilTest.java
+++ DFSPDirectoryGateway/src/test/java/com/l1p/interop/JSONUtilTest.java
@@ -10,14 +10,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * JUNit Test for JSONUtil.  This test currently only tests failure modes.
+ * JUNit Test for JSONUtil.
  *
  * Created by Bryan on 8/18/2016.
  */
 public class JSONUtilTest {
 
     @Test
-    public void invalidInputShouldProduceRuntimeException() throws Exception {
+    public void testStringToMap() throws Exception {
+        final String jsonInput = "{\"key1\":\"value1\",\"Key2\":\"Value2\",\"key3\":null,\"key4\":{\"nestedKey2\":null,\"nestedKey1\":\"nestedValue1\"}}";
+        //verify output converts back to its source
+        try {
+            Map target = JSONUtil.stringToMap(jsonInput);
+            assertTrue( "Output map not of expected size, expected = 4, actual = " + target.size(), target.size() == 4 );
+            assertEquals( "Value for key1 did not match expected", "value1", target.get( "key1" ) );
+            assertEquals( "Value for Key2 did not match expected", "Value2", target.get( "Key2" ) );
+            assertEquals( "Value for key3 did not match expected", null, target.get( "key3" ) );
+
+            Map nestedMap = (Map)target.get( "key4" );
+            assertTrue( "Nested map not of expected size, expected = 2, actual = " + nestedMap.size(), nestedMap.size() == 2 );
+            assertEquals( "Value for nestedKey1 did not match expected", "nestedValue1", nestedMap.get( "nestedKey1" ) );
+            assertEquals( "Value for nestedKey2 did not match expected", null, nestedMap.get( "nestedKey2" ) );
+
+        } catch( Exception e ) {
+            fail( "Conversion of JSON input to a map produced an unexpected exception: " + e.getMessage() );
+        }
+
         //test map creation from non-json input
         try {
             JSONUtil.stringToMap( "<NotJSON/>" );
@@ -31,13 +49,55 @@ public class JSONUtilTest {
 
         //test map creation from null input - should return empty Map
         Map<String,Object> map = JSONUtil.stringToMap( null );
-        assertTrue( "Map produced from null input was not empty", map.isEmpty() );
+        try {
+            assertTrue("Map produced from null input was not empty", map.isEmpty());
+        } catch( Exception e ) {
+            fail( "Creation of a map from null input produced an unexpected exception: " + e.getMessage() );
+        }
+    }
 
-        //test string creation from null input
-        assertEquals( "Conversion of null data to string produced unexpected result", JSONUtil.mapToString( null ), "{}" );
+    @Test
+    public void testMapToString() throws Exception {
 
+        //verify good input produces output that converts back to a Map via JSON
+        Map<String,Object> source = new HashMap<String,Object>();
+        source.put( "key1", "value1" );
+        source.put( "Key2", "Value2" );
+        source.put( "key3", null );
+
+        Map<String,Object> nestedMap = new HashMap<String,Object>();
+        nestedMap.put( "nestedKey1", "nestedValue1" );
+        nestedMap.put( "nestedKey2", null );
+
+        source.put( "key4", nestedMap );
+
+        String jsonResult = null;
+        try {
+            jsonResult = JSONUtil.mapToString(source);
+        } catch( Exception e ) {
+            fail( "Conversion of a valid map to a JSON string produced an unexpected exception: " + e.getMessage() );
+        }
+
+        //verify output converts back to its source
+        try {
+            Map target = JSONUtil.stringToMap(jsonResult);
+            assertTrue( "Output map not of expected size, expected = 4, actual = " + target.size(), target.size() == 4 );
+            assertEquals( "Value for key1 did not match expected", "value1", target.get( "key1" ) );
+            assertEquals( "Value for Key2 did not match expected", "Value2", target.get( "Key2" ) );
+            assertEquals( "Value for key3 did not match expected", null, target.get( "key3" ) );
+
+            nestedMap = (Map)target.get( "key4" );
+            assertTrue( "Nested map not of expected size, expected = 2, actual = " + nestedMap.size(), nestedMap.size() == 2 );
+            assertEquals( "Value for nestedKey1 did not match expected", "nestedValue1", nestedMap.get( "nestedKey1" ) );
+            assertEquals( "Value for nestedKey2 did not match expected", null, nestedMap.get( "nestedKey2" ) );
+
+        } catch( Exception e ) {
+            fail( "Conversion of JSON output back to a map produced an unexpected exception: " + e.getMessage() );
+        }
+
+        //verify bad input produces the expected exception
         Map badDataMap = new HashMap();
-        badDataMap.put( "key", System.err );
+        badDataMap.put( "key", System.err );    //map contains an object that is not serializable as JSON
         try {
             JSONUtil.mapToString( badDataMap );
         } catch( NullPointerException e ) {
@@ -46,6 +106,13 @@ public class JSONUtilTest {
             assertTrue( "Conversion of invalid map data to string produced a RuntimeException of an unexpected type: " + e.getMessage(), e.getMessage().contains( "Failed to convert source Map to JSON" ) );
         } catch( Exception e ) {
             fail( "Conversion of invalid map data to string produced an unexpected exception: " + e.getClass().getName() );
+        }
+
+        //test string creation from null input
+        try {
+            assertEquals("Conversion of null data to string produced unexpected result", JSONUtil.mapToString(null), "{}");
+        } catch( Exception e ) {
+            fail( "Conversion of null data to a string produced an unexpected exception: " + e.getMessage() );
         }
     }
 }
