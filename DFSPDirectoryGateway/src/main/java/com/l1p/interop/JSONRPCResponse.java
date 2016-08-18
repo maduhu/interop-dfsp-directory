be69ede680ed7f4c53016d327396085528b3c6cc
Joe Gaber <joe@durangoitarchitects.com>
2016-08-18 12:49:21 -0700

    added comment

--- DFSPDirectoryGateway/src/main/java/com/l1p/interop/JSONRPCResponse.java
+++ DFSPDirectoryGateway/src/main/java/com/l1p/interop/JSONRPCResponse.java
@@ -1,37 +1,39 @@
 package com.l1p.interop;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * This class's constructor adds the needed header details to comply with
+ * JSONRpc conventions and is used as the response object to JSONRpc commands.
+ * 
  * Created by Bryan on 8/17/2016.
  */
 public class JSONRPCResponse {
 
-    public static final String ID_FIELD = "id";
-    public static final String RESULT_FIELD = "result";
+	public static final String ID_FIELD = "id";
+	public static final String RESULT_FIELD = "result";
 
-    final String responseJSON;
+	final String responseJSON;
 
-    public JSONRPCResponse( String id, Map result ) {
-        Map header = new HashMap();
+	public JSONRPCResponse(String id, Map result) {
+		Map header = new HashMap();
 
-        if ( result == null )
-            result = new HashMap();
+		if (result == null)
+			result = new HashMap();
 
-        if ( id == null )
-            id = "";
+		if (id == null)
+			id = "";
 
-        header.put( "jsonrpc", "2.0" );
-        header.put( ID_FIELD, id );
-        header.put( RESULT_FIELD, result );
+		header.put("jsonrpc", "2.0");
+		header.put(ID_FIELD, id);
+		header.put(RESULT_FIELD, result);
 
-        responseJSON = JSONUtil.mapToString( header );
-    }
+		responseJSON = JSONUtil.mapToString(header);
+	}
 
-    @Override
-    public String toString() {
-        return responseJSON;
-    }
+	@Override
+	public String toString() {
+		return responseJSON;
+	}
 }
